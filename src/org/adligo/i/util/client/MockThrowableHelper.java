05d2daf0ca87de3a898f6f2ace9fb31ee365a5d4
scott <scott@adligo.com>
2012-01-28 17:47:24 +0000

    this commit will break the entire adligo build, but is necessary to allow for indenting log messages in the new rolling_file_appender project

--- src/org/adligo/i/util/client/MockThrowableHelper.java
+++ src/org/adligo/i/util/client/MockThrowableHelper.java
@@ -3,17 +3,55 @@ package org.adligo.i.util.client;
 import org.adligo.i.util.client.I_ThrowableHelper;
 
 public class MockThrowableHelper implements I_ThrowableHelper {
-
+	private Throwable lastFillInStackTraceThrowable;
+	
+	private Throwable lastGetStackTraceAsStringThrowable;
+	private String lastGetStackTraceAsStringPreText;
+	private String lastGetStackTraceAsStringLineFeed;
+	private String nextStackTraceAsStringReturn = null;
+	
 	@Override
 	public void fillInStackTrace(Throwable p) {
-		// TODO Auto-generated method stub
-		
+		lastFillInStackTraceThrowable =p;
 	}
 
 	@Override
 	public String getStackTraceAsString(Throwable p) {
-		// TODO Auto-generated method stub
-		return null;
+		lastGetStackTraceAsStringThrowable = p;
+		return nextStackTraceAsStringReturn;
+	}
+
+	@Override
+	public String getStackTraceAsString(String preText, Throwable p,
+			String lineFeed) {
+		lastGetStackTraceAsStringThrowable = p;
+		lastGetStackTraceAsStringPreText = preText;
+		lastGetStackTraceAsStringLineFeed = lineFeed;
+		return nextStackTraceAsStringReturn;
+	}
+
+	public String getNextStackTraceAsStringReturn() {
+		return nextStackTraceAsStringReturn;
+	}
+
+	public void setNextStackTraceAsStringReturn(String nextStackTraceAsStringReturn) {
+		this.nextStackTraceAsStringReturn = nextStackTraceAsStringReturn;
+	}
+
+	public Throwable getLastFillInStackTraceThrowable() {
+		return lastFillInStackTraceThrowable;
+	}
+
+	public Throwable getLastGetStackTraceAsStringThrowable() {
+		return lastGetStackTraceAsStringThrowable;
+	}
+
+	public String getLastGetStackTraceAsStringPreText() {
+		return lastGetStackTraceAsStringPreText;
+	}
+
+	public String getLastGetStackTraceAsStringLineFeed() {
+		return lastGetStackTraceAsStringLineFeed;
 	}
 
 }
