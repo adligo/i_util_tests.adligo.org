e8cad0a95da4ec30b93b8e600f4603e09b904b39
scott <scott@adligo.com>
2012-05-11 00:53:14 +0000

    new classes

---
+++ src/org/adligo/i/util/test_utils/LineText.java
@@ -0,0 +1,48 @@
+package org.adligo.i.util.test_utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineText {
+	private List<String> lines = new ArrayList<String>();
+	
+	public LineText(String text) {
+		char [] chars = text.toCharArray();
+		StringBuilder line = new StringBuilder();
+		boolean previousLineFeed = false;
+		
+		for (int i = 0; i < chars.length; i++) {
+			char c = chars[i];
+			if (c == '\n') {
+				if (!previousLineFeed) {
+					String lineText = line.toString();
+					line = new StringBuilder();
+					lines.add(lineText);
+				}
+				previousLineFeed = true;
+			} else if (c == '\r') {
+				if (!previousLineFeed) {
+					String lineText = line.toString();
+					line = new StringBuilder();
+					lines.add(lineText);
+				}
+				previousLineFeed = true;
+			} else {
+				line.append(c);
+				previousLineFeed = false;
+			}
+		}
+		String lineText = line.toString();
+		if (lineText.length() != 0) {
+			lines.add(lineText);
+		}
+	}
+	
+	public int getLines() {
+		return lines.size();
+	}
+	
+	public String getLine(int i) {
+		return lines.get(i);
+	}
+}
---
+++ src/org/adligo/i/util/test_utils/LineTextAssertions.java
@@ -0,0 +1,28 @@
+package org.adligo.i.util.test_utils;
+
+import junit.framework.Assert;
+
+public class LineTextAssertions {
+	public static void compaireFileText(String example, String actual) {
+		LineText exampleLT = new LineText(example);
+		LineText actualLT = new LineText(actual);
+		Assert.assertEquals("The number of lines should match expected ",
+				 exampleLT.getLines(), actualLT.getLines());
+		
+		for (int i = 0; i < exampleLT.getLines(); i++) {
+			char [] exampleChars = exampleLT.getLine(i).toCharArray();
+			char [] actualChars = actualLT.getLine(i).toCharArray();
+			for (int j = 0; j < exampleChars.length; j++) {
+				char c = exampleChars[j];
+				char a = actualChars[j];
+				if (c != a) {
+					Assert.assertEquals("the character on line " + i + " at character " + j +
+							" should match",example, actual);
+				}
+			}
+			
+			Assert.assertEquals("The characters in line " + i + " should match ", 
+					exampleChars.length, actualChars.length);
+		}
+	}
+}
