1a84702d5587df4eb1f6d2bfc5cdbb54ca30b1e5
scott <scott@adligo.com>
2012-06-02 01:35:06 +0000

    fixing test

--- src/org/adligo/i/util/client/UtilEntryPoint.java
+++ src/org/adligo/i/util/client/UtilEntryPoint.java
@@ -280,6 +280,9 @@ public class UtilEntryPoint implements EntryPoint {
 		addI_ThrowableHelper();
 		addI_Wrapper();
 		holder.addUsed(I_Immutable.class);
+		holder.addUsed(I_DateTimeParser.class);
+		holder.addUsed(DateTimeParserFactory.class);
+		
 		holder.addUsed(InstanceForName.class);
 		holder.addUsed(IteratorFactory.class);
 		holder.addUsed(new ListenerValueObject());
