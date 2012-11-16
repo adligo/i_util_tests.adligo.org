b0195dd994e0c6cfce9ac92e053e051ed2996285
scott <scott@adligo.com>
2012-11-16 21:48:14 +0000

    adding tests

--- src/org/adligo/i/util/DateTimeTests.java
+++ src/org/adligo/i/util/DateTimeTests.java
@@ -17,7 +17,14 @@ public class DateTimeTests extends ATest {
 		assertEquals(2001,dt.getYear());
 		assertEquals(1, dt.getMonth());
 		assertEquals(29, dt.getDayOfMonth());
-		
+		assertEquals(980748060000L, dt.getTime());
+		assertEquals(new Long(980748060000L), dt.getTimeLong());
+	}
+	
+	public void testDayStartEnd() {
+		DateTime dt = new DateTime(0);
+		assertEquals(-64800000L,dt.getDayStart());
+		assertEquals(21540999L,dt.getDayEnd());
 	}
 	
 	public void testYear() {
@@ -36,6 +43,9 @@ public class DateTimeTests extends ATest {
 	
 	public void testDayOfMonth() {
 		DateTime dt = new DateTime(0);
+		assertEquals(0L, dt.getTime());
+		assertEquals(new Long(0L), dt.getTimeLong());
+		
 		assertEquals(31,dt.getDayOfMonth());
 		dt = new DateTime(DateTime.ONE_DAY);
 		assertEquals(1,dt.getDayOfMonth());
@@ -105,12 +115,26 @@ public class DateTimeTests extends ATest {
 		assertEquals(DateTime.SUNDAY,dt.getDayOfWeek());
 	}
 	
+	
 	public void testToString() throws Exception {
 		assertEquals("12/31/1969 06:00 PM 000", new DateTime(0).toString());
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 		Date date = sdf.parse("01/01/2000");
 		assertEquals("01/01/2000 12:00 AM 000", new DateTime(date.getTime()).toString());
+		
+		DateTimeExtension dt = new DateTimeExtension();
+		assertEquals("01/01/2000 12:00 AM 000",  dt.toStringFix("01/01/2000 12:00 aa.m. 000"));
+		assertEquals("01/01/2000 12:00 PM 000",  dt.toStringFix("01/01/2000 12:00 ap.m. 000"));
+		
+	}
+	
+	public void testFormatDateMethods() throws Exception {
+		assertEquals("12/31/1969", DateTime.formatDate(0L));
+		assertEquals("12/31/1969 06:00 PM 000", DateTime.formatDateTime(0L));
+		assertEquals("12/31/1969", DateTime.formatDate(DateTime.FULL_YEAR_DATE_FORMAT, 0L));
+		
+		
 	}
 	
 	public void testDayOfYearAllParmas() throws Exception {
@@ -200,8 +224,26 @@ public class DateTimeTests extends ATest {
 	
 
 	public void testGetDaysInMonth() {
+		assertEquals(31, DateTime.getDaysInMonth(DateTime.DECEMBER, 2001));
 		assertEquals(29, DateTime.getDaysInMonth(DateTime.FEBUARY, 2000));
 		assertEquals(28, DateTime.getDaysInMonth(DateTime.FEBUARY, 2001));
+		assertEquals(31, DateTime.getDaysInMonth(DateTime.MARCH, 2001));
+		assertEquals(30, DateTime.getDaysInMonth(DateTime.APRIL, 2001));
+		assertEquals(31, DateTime.getDaysInMonth(DateTime.MAY, 2001));
+		assertEquals(30, DateTime.getDaysInMonth(DateTime.JUNE, 2001));
+		assertEquals(31, DateTime.getDaysInMonth(DateTime.JULY, 2001));
+		assertEquals(31, DateTime.getDaysInMonth(DateTime.AUGUST, 2001));
+		assertEquals(30, DateTime.getDaysInMonth(DateTime.SEPTEMBER, 2001));
+		assertEquals(31, DateTime.getDaysInMonth(DateTime.OCTOBER, 2001));
+		assertEquals(31, DateTime.getDaysInMonth(DateTime.DECEMBER, 2001));
+		IllegalArgumentException caught = null;
+		try {
+			DateTime.getDaysInMonth((short) 13, 2001);
+		} catch (IllegalArgumentException e) {
+			caught = e;
+		}
+		assertNotNull(caught);
+		assertEquals(DateTime.THE_MONTH_PASSED_IN_TO_GET_DAYS_IN_MONT_IS_NOT_BETWEEN_1_AND_12, caught.getMessage());
 	}
 	
 	public void testDaysYearMonth() {
